43b021ebf8e7da710ff54ce8fc7c4050744f93dc
Torsten Förtsch <torsten.foertsch@gmx.net>
2007-05-27 11:33:00 +0000

    Import of OPI/IO-Handle-Record-0.06 from CPAN.
    gitpan-cpan-distribution: IO-Handle-Record
    gitpan-cpan-version:      0.06
    gitpan-cpan-path:         OPI/IO-Handle-Record-0.06.tar.gz
    gitpan-cpan-author:       OPI
    gitpan-cpan-maturity:     released

--- lib/IO/Handle/Record.pm
+++ lib/IO/Handle/Record.pm
@@ -4,43 +4,154 @@ use 5.008;
 use strict;
 use warnings;
 use Storable;
-use Class::Member::GLOB qw/record_opts/;
+use Class::Member::GLOB qw/record_opts read_buffer expected
+			   write_buffer written/;
+use Errno qw/EAGAIN EINTR/;
+use Carp;
 
-our $VERSION = '0.03';
+our $VERSION = '0.06';
 
 sub read_record {
   my $I=shift;
-  my $buf='';
-  my $len=$I->sysread( $buf, 4 );
-  return if( $len==0 );			# EOF: ok
-  die "IO::Handle::Record: Protocol Error (got $len bytes, expected 4)"
-    unless( $len==4 );
-  $len=unpack "L", $buf;
-  $buf='';
-  my ($buflen,$x)=(0);
-  while( defined($x=$I->sysread( $buf, $len-$buflen, $buflen )) ) {
-	$buflen+=$x;
-	last if( $buflen>=$len );
+
+  unless( defined $I->expected ) {
+    $I->read_buffer='' unless( defined $I->read_buffer );
+    my $buflen=length($I->read_buffer);
+    while( $buflen<4 ) {
+      my $len=$I->sysread( $I->read_buffer, 4-$buflen, $buflen );
+      if( defined($len) && $len==0 ) { # EOF
+	undef $I->read_buffer;
+	return;
+      } elsif( !defined($len) && $!==EAGAIN ) {
+	return;			# non blocking file handle
+      } elsif( !defined($len) && $!==EINTR ) {
+	next;			# interrupted
+      } elsif( !$len ) {	# ERROR
+	$len=length $I->read_buffer;
+	undef $I->read_buffer;
+	croak "IO::Handle::Record: Protocol Error (got $len bytes, expected 4)";
+      }
+      $buflen+=$len;
+    }
+    $I->expected=unpack "L", $I->read_buffer;
+    $I->read_buffer='';
   }
-  die "IO::Handle::Record: Protocol Error (got $buflen bytes, expected $len)"
-	unless( $buflen==$len );
-  local $Storable::Eval;
-  $I->record_opts and $Storable::Eval=$I->record_opts->{receive_CODE};
-  return @{Storable::thaw( $buf )};
+
+  my $wanted=$I->expected;
+  my $buflen=length($I->read_buffer);
+  while( $buflen<$wanted ) {
+    my $len=$I->sysread( $I->read_buffer, $wanted-$buflen, $buflen );
+    if( !defined($len) && $!==EAGAIN ) {
+      return;			# non blocking file handle
+    } elsif( !defined($len) && $!==EINTR ) {
+      next;			# interrupted
+    } elsif( !$len ) {		# EOF or ERROR
+      $len=length $I->read_buffer;
+      undef $I->read_buffer;
+      croak "IO::Handle::Record: Protocol Error (got $len bytes, expected $wanted)";
+    }
+    $buflen+=$len;
+  }
+
+  my $rc=eval {
+    local $Storable::Eval;
+    $I->record_opts and $Storable::Eval=$I->record_opts->{receive_CODE};
+    Storable::thaw( $I->read_buffer );
+  };
+  if( $@ ) {
+    my $e=$@;
+    $e=~s/ at .*//s;
+    croak $e;
+  }
+
+  undef $I->expected;
+  undef $I->read_buffer;
+
+  return @{$rc};
 }
 
 sub write_record {
   my $I=shift;
-  local $Storable::Deparse;
-  $I->record_opts and $Storable::Deparse=$I->record_opts->{send_CODE};
-  my $msg=Storable::nfreeze \@_;
-  $I->print( pack( "L", length($msg) ), $msg );
+
+  if( @_ ) {
+    my $msg=eval {
+      local $Storable::Deparse;
+      $I->record_opts and $Storable::Deparse=$I->record_opts->{send_CODE};
+      Storable::nfreeze \@_;
+    };
+    if( $@ ) {
+      my $e=$@;
+      $e=~s/ at .*//s;
+      croak $e;
+    }
+
+    $I->write_buffer=pack( "L", length($msg) ).$msg;
+    $I->written=0;
+  }
+
+  my $written;
+  while( $I->written<length($I->write_buffer) and
+	 (defined ($written=$I->syswrite( $I->write_buffer,
+					  length($I->write_buffer)-$I->written,
+					  $I->written)) or
+	  $!==EINTR) ) {
+    $I->written+=$written if( defined $written );
+  }
+  if( $I->written==length($I->write_buffer) ) {
+    undef $I->write_buffer;
+    undef $I->written;
+    return 1;
+  } elsif( $!==EAGAIN ) {
+    return;
+  } else {
+    croak "IO::Handle::Record: syswrite error";
+  }
+}
+
+sub read_simple_record {
+  my $I=shift;
+  local $/;
+  my $delim;
+  if( $I->record_opts ) {
+    $/=$I->record_opts->{record_delimiter} || "\n";
+    $delim=$I->record_opts->{field_delimiter} || "\0";
+  } else {
+    $/="\n";
+    $delim="\0";
+  }
+
+  my $r=<$I>;
+  return unless( defined $r );	# EOF
+
+  chomp $r;
+  return split /\Q$delim\E/, $r;
+}
+
+sub write_simple_record {
+  my $I=shift;
+  my $rdelim;
+  my $delim;
+  if( $I->record_opts ) {
+    $rdelim=$I->record_opts->{record_delimiter} || "\n";
+    $delim=$I->record_opts->{field_delimiter} || "\0";
+  } else {
+    $rdelim="\n";
+    $delim="\0";
+  }
+
+  print( $I join( $delim , @_ ), $rdelim );
   $I->flush;
 }
 
 *IO::Handle::write_record=\&write_record;
 *IO::Handle::read_record=\&read_record;
+*IO::Handle::write_simple_record=\&write_simple_record;
+*IO::Handle::read_simple_record=\&read_simple_record;
 *IO::Handle::record_opts=\&record_opts;
+*IO::Handle::expected=\&expected;
+*IO::Handle::read_buffer=\&read_buffer;
+*IO::Handle::written=\&written;
+*IO::Handle::write_buffer=\&write_buffer;
 
 1;
 __END__
@@ -87,7 +198,7 @@ The following methods are added:
 
 =over 4
 
-=item B<record_opts>
+=item B<$handle-E<gt>record_opts>
 
 This lvalue method expects a hash reference with options as parameter.
 The C<send_CODE> and C<receive_CODE> options are defined. They correspond
@@ -98,9 +209,18 @@ Example:
 
  $handle->record_opts={send_CODE=>1, receive_CODE=>1};
 
-=item B<write_record>
+=item B<$handle-E<gt>write_record(@data)>
 
 writes a list of perl data structures.
+
+C<write_record> returns 1 if the record has been transmitted. C<undef> is
+returned if C<$handle> is non blocking and a EAGAIN condition is met. In
+this case reinvoke the operation without parameters
+(just C<$handle-E<gt>write_record>) when the handle becomes ready.
+Otherwise it throws an exception C<IO::Handle::Record: syswrite error>.
+Check C<$!> in this case.
+
+EINTR is handled internally.
 
 Example:
 
@@ -110,13 +230,33 @@ Example:
                           hash=>{a=>'b'},
                           code=>sub {print "test\n";} } );
 
-=item B<read_record>
+=item B<@data=$handle-E<gt>read_record>
 
-reads one record of perl data structures. Returns the list.
+reads one record of perl data structures.
+
+On success it returns the record as list. An empty list is returned if
+C<$handle> is in non blocking mode and not enough data has been read.
+Check $!==EAGAIN to catch this condition. When the handle becomes ready
+just repeat the operation to read the next data chunk. If a complete record
+has arrived it is returned.
+
+On EOF an empty list is returned. To distinguish this from the non blocking
+empty list return set C<$!=0> before the operation and check for C<$!==EAGAIN>
+after.
+
+EINTR is handled internally.
 
 Example:
 
  ($array, $sub, $hash)=$handle->read_record;
+
+=item B<$handle-E<gt>read_buffer>
+=item B<$handle-E<gt>expected>
+=item B<$handle-E<gt>write_buffer>
+=item B<$handle-E<gt>written>
+
+these methods are used internally to provide a read and write buffer for
+non blocking operations.
 
 =back
 
--- t/IO-Handle-Record.t
+++ t/IO-Handle-Record.t
@@ -1,16 +1,12 @@
-# Before `make install' is performed this script should be runnable with
-# `make test'. After `make install' it should work as `perl IO-Socket-Record.t'
-
-#########################
-
-# change 'tests => 1' to 'tests => last_test_to_print';
-
-use Test::More tests => 11;
+use Test::More tests => 16;
+use Test::Deep;
 use Data::Dumper;
 $Data::Dumper::Deparse=1;
 BEGIN { use_ok('IO::Handle::Record') };
 use IO::Socket::UNIX;
 use IO::Pipe;
+use IO::Select;
+use Errno qw/EAGAIN/;
 
 #########################
 
@@ -18,7 +14,13 @@ sub t {
   my ($got, $expected, $name)=@_;
   my $rc=($got eq $expected);
   local $_;
-  ($got, $expected, $name)=map {s/\n$//;s/\n/\n#/g;$_} ($got, $expected, $name);
+  ($got, $expected, $name)=map {
+    if( defined $_ ) {
+      s/\n$//;s/\n/\n#/g;$_;
+    } else {
+      "UNDEFINED";
+    }
+  } ($got, $expected, $name);
   print "# testing: $name\n";
   print "# expected: $expected\n";
   print "# received: $got\n";
@@ -28,7 +30,7 @@ sub t {
 my ($p, $c)=(IO::Pipe->new, IO::Pipe->new);
 
 my $pid;
-while( !defined( $pid=fork ) ) {sleep 1}
+while( !defined( $pid=fork ) ) {select undef, undef, undef, .2}
 
 if( $pid ) {
   $p->reader; $c->writer;
@@ -36,35 +38,42 @@ if( $pid ) {
 
   $c->write_record( 1 );
   ($got)=$p->read_record;
-  t $got, Dumper( [1] ), 'simple scalar';
+  cmp_deeply $got, [1], 'simple scalar';
+
+  $c->write_record( 'string' );
+  ($got)=$p->read_record;
+  cmp_deeply $got, ['string'], 'simple string';
 
   $c->write_record( 1, 2, 3, 4 );
   ($got)=$p->read_record;
-  t $got, Dumper( [1, 2, 3, 4] ), 'scalar list';
+  cmp_deeply $got, [1, 2, 3, 4], 'scalar list';
 
   $c->write_record( [1,2], [3,4] );
   ($got)=$p->read_record;
-  t $got, Dumper( [[1, 2], [3, 4]] ), 'list list';
+  cmp_deeply $got, [[1, 2], [3, 4]], 'list list';
 
   $c->write_record( [1,2], +{a=>'b', c=>'d'} );
   ($got)=$p->read_record;
-  t $got, Dumper( [[1, 2], +{a=>'b', c=>'d'}] ), 'list+hash list';
+  cmp_deeply $got, [[1, 2], +{a=>'b', c=>'d'}], 'list+hash list';
 
   $c->record_opts={send_CODE=>1};
+  $p->record_opts={receive_CODE=>sub {eval $_[0]}};
   $c->write_record( +{a=>'b', c=>'d'}, sub { $_[0]+$_[1] } );
   ($got)=$p->read_record;
-  t $got, Dumper( [+{a=>'b', c=>'d'}, sub { $_[0]+$_[1] }] ), 'hash+sub list';
+  cmp_deeply Dumper( $got ), Dumper( [+{a=>'b', c=>'d'}, sub { $_[0]+$_[1] }] ),
+             'hash+sub list';
 } else {
   $c->reader; $p->writer;
   $c->record_opts={receive_CODE=>sub {eval $_[0]}};
+  $p->record_opts={send_CODE=>1};
   while( my @l=$c->read_record ) {
-    $p->write_record( Dumper( \@l ) );
+    $p->write_record( \@l  );
   }
   exit 0;
 }
 
 ($p, $c)=IO::Socket::UNIX->socketpair( AF_UNIX,SOCK_STREAM,PF_UNSPEC );
-while( !defined( $pid=fork ) ) {sleep 1}
+while( !defined( $pid=fork ) ) {select undef, undef, undef, .1}
 
 if( $pid ) {
   close $c; undef $c;
@@ -72,29 +81,101 @@ if( $pid ) {
 
   $p->write_record( 1 );
   ($got)=$p->read_record;
-  t $got, Dumper( [1] ), 'simple scalar';
+  cmp_deeply $got, [1], 'simple scalar';
 
   $p->write_record( 1, 2, 3, 4 );
   ($got)=$p->read_record;
-  t $got, Dumper( [1, 2, 3, 4] ), 'scalar list';
+  cmp_deeply $got, [1, 2, 3, 4], 'scalar list';
 
   $p->write_record( [1,2], [3,4] );
   ($got)=$p->read_record;
-  t $got, Dumper( [[1, 2], [3, 4]] ), 'list list';
+  cmp_deeply $got, [[1, 2], [3, 4]], 'list list';
 
   $p->write_record( [1,2], +{a=>'b', c=>'d'} );
   ($got)=$p->read_record;
-  t $got, Dumper( [[1, 2], +{a=>'b', c=>'d'}] ), 'list+hash list';
+  cmp_deeply $got, [[1, 2], +{a=>'b', c=>'d'}], 'list+hash list';
 
-  $p->record_opts={send_CODE=>1};
+  $p->record_opts={receive_CODE=>sub {eval $_[0]}, send_CODE=>1};
   $p->write_record( +{a=>'b', c=>'d'}, sub { $_[0]+$_[1] } );
   ($got)=$p->read_record;
-  t $got, Dumper( [+{a=>'b', c=>'d'}, sub { $_[0]+$_[1] }] ), 'hash+sub list';
+  cmp_deeply Dumper( $got ), Dumper( [+{a=>'b', c=>'d'}, sub { $_[0]+$_[1] }] ),
+             'hash+sub list';
 } else {
   close $p; undef $p;
-  $c->record_opts={receive_CODE=>sub {eval $_[0]}};
+  $c->record_opts={receive_CODE=>sub {eval $_[0]}, send_CODE=>1};
   while( my @l=$c->read_record ) {
-    $c->write_record( Dumper( \@l ) );
+    $c->write_record( \@l );
+  }
+  exit 0;
+}
+
+($p, $c)=(IO::Pipe->new, IO::Pipe->new);
+while( !defined( $pid=fork ) ) {select undef, undef, undef, .1}
+
+if( $pid ) {
+  $p->reader; $c->writer;
+  my $got;
+  my $msg=Storable::nfreeze( [1, 2] );
+  $msg=pack( "L", length($msg) ).$msg;
+  for( my $i=0; $i<length $msg; $i++ ) {
+    $c->syswrite( $msg, 1, $i );
+    select undef, undef, undef, 0.1;
+  }
+  my $again;
+  ($got, $again)=$p->read_record;
+  cmp_deeply $got, [1, 2], 'nonblocking read';
+
+  cmp_deeply $again, code(sub{$_[0]>0 ? 1 : (0, "expected >0, got $_[0]")}),
+             'again>0';
+} else {
+  $c->reader; $p->writer;
+  $c->blocking(0);
+  my $sel=IO::Select->new($c);
+
+  my $again=0;
+  while( $sel->can_read ) {
+    $!=0;
+    my @l=$c->read_record;
+    if( @l ) {
+      $p->write_record( \@l, $again );
+    } elsif( $!==EAGAIN ) {
+      $again++;
+    } else {
+      last;
+    }
+  }
+  exit 0;
+}
+
+($p, $c)=(IO::Pipe->new, IO::Pipe->new);
+
+while( !defined( $pid=fork ) ) {select undef, undef, undef, .2}
+
+if( $pid ) {
+  $p->reader; $c->writer;
+  my $got;
+  $c->blocking(0);
+  my $sel=IO::Select->new($c);
+
+  my $again=0;
+  if( $sel->can_write and !$c->write_record( ('xyzabc123')x30000 ) ) {
+    $again++;
+    while( $sel->can_write and !$c->write_record ) {
+      $again++;
+    }
+  }
+
+  ($got)=$p->read_record;
+  cmp_deeply $got, [('xyzabc123')x30000], 'nonblocking write';
+
+  print "# again=$again\n";
+  cmp_deeply $again, code(sub{$_[0]>0 ? 1 : (0, "expected >0, got $_[0]")}),
+             'again>0';
+} else {
+  $c->reader; $p->writer;
+  select undef, undef, undef, 0.5;
+  while( my @l=$c->read_record ) {
+    $p->write_record( \@l );
   }
   exit 0;
 }
